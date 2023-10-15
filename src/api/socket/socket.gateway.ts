4b4201f12a4e206110a570a9efa0a0e5be0f4290
i8lex <cybeerid@gmail.com>
2023-10-15 18:35:29 +0300

    change chat logic in socket.gateway.ts

--- src/api/socket/socket.gateway.ts
+++ src/api/socket/socket.gateway.ts
@@ -26,15 +26,20 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     private readonly userService: UserService,
   ) {}
 
+  private userSocketMap = new Map<string, string>();
+
   async handleConnection(client: Socket) {
     try {
       if (client.handshake.headers['authorization']) {
         const user = await this.chatService.getUserFromSocket(client);
+        this.userSocketMap.set(user.userId, client.id);
+
         await this.userService.setStatus(user.userId, true);
         this.server.emit('userState', {
           userId: user.userId,
           isOnline: true,
         });
+        console.log('User is online', client.id);
       }
     } catch (err) {
       console.error(err);
@@ -49,76 +54,35 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
         userId: user.userId,
         isOnline: false,
       });
+      console.log('User is offline', client.id);
     }
   }
 
-  @SubscribeMessage('join-room')
-  async handleJoinRoom(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() body: string,
-  ) {
-    try {
-      console.log(`Joined room: ${body}`);
-      client.join(body);
-    } catch (error) {
-      console.error('Error in userState handler:', error);
-    }
-  }
-
-  @SubscribeMessage('leave-room')
-  async handleLeaveRoom(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() body: string,
-  ) {
-    try {
-      console.log(`Leaved room: ${body}`);
-      client.leave(body);
-    } catch (error) {
-      console.error('Error in userState handler:', error);
-    }
-  }
-
-  @SubscribeMessage('userState')
-  async handleUserState(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() body: boolean,
-  ) {
-    try {
-      if (client.handshake.headers['authorization']) {
-        const user = await this.chatService.getUserFromSocket(client);
-
-        await this.userService.setStatus(user.userId, body);
-        this.server.emit('userState', {
-          userId: user.userId,
-          isOnline: body,
-        });
-      }
-    } catch (error) {
-      console.error('Error in userState handler:', error);
-    }
-  }
-
-  @SubscribeMessage('isDeliveredMessage')
-  async handleDeliverMessage(
-    @MessageBody() body: MessageDto,
-    @ConnectedSocket() client: Socket,
-  ) {
-    const user = await this.chatService.getUserFromSocket(client);
-    const roomName = client.handshake.query.room;
-    const deliverTo = await this.chatService.deliverMessage(body, user.userId);
-    this.server.to(roomName).emit('isDeliveredMessage', deliverTo.userId);
-  }
-
-  @SubscribeMessage('readMessage')
-  async handleReadMessage(
-    @MessageBody() body: MessageDto,
-    @ConnectedSocket() client: Socket,
-  ) {
-    const user = await this.chatService.getUserFromSocket(client);
-    const roomName = client.handshake.query.room;
-    const readBy = await this.chatService.readBy(body, user.userId);
-    this.server.to(roomName).emit('readMessage', readBy);
-  }
+  // @SubscribeMessage('join-room')
+  // async handleJoinRoom(
+  //   @ConnectedSocket() client: Socket,
+  //   @MessageBody() body: string,
+  // ) {
+  //   try {
+  //     client.join(body);
+  //     console.log(client.rooms, 'rooms after');
+  //   } catch (error) {
+  //     console.error('Error in userState handler:', error);
+  //   }
+  // }
+  //
+  // @SubscribeMessage('leave-room')
+  // async handleLeaveRoom(
+  //   @ConnectedSocket() client: Socket,
+  //   @MessageBody() body: string,
+  // ) {
+  //   try {
+  //     client.leave(body);
+  //     console.log(`Leaved room: ${body}`, client.rooms);
+  //   } catch (error) {
+  //     console.error('Error in userState handler:', error);
+  //   }
+  // }
 
   @SubscribeMessage('chatMessage')
   async handleMessage(
@@ -128,8 +92,40 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     try {
       const user = await this.chatService.getUserFromSocket(client);
       body.username = user.username;
-      const message = await this.chatService.saveMessage(body);
-      this.server.to(body.chatId).emit('chatMessage', message);
+
+      if (!body.event) {
+        const message = await this.chatService.saveMessage(body);
+
+        body.users.forEach((userId) => {
+          const socketId = this.userSocketMap.get(userId);
+          this.server.to(socketId).emit('chatMessage', message);
+          if (userId !== user.userId) {
+            this.server.to(socketId).emit('counter', {
+              _id: message._id,
+              chatId: message.chatId,
+              users: body.users,
+            });
+          }
+        });
+      }
+      if (body.event === 'deliver') {
+        const data = await this.chatService.deliverMessage(body, user.userId);
+        body.users.forEach((userId) => {
+          if (userId !== user.userId) {
+            const socketId = this.userSocketMap.get(userId);
+            this.server.to(socketId).emit('chatMessage', data);
+          }
+        });
+      }
+      if (body.event === 'read') {
+        const data = await this.chatService.readBy(body, user.userId);
+        body.users.forEach((userId) => {
+          if (userId !== user.userId) {
+            const socketId = this.userSocketMap.get(userId);
+            this.server.to(socketId).emit('chatMessage', data);
+          }
+        });
+      }
     } catch (error) {
       console.error('Error in message handler:', error);
     }
